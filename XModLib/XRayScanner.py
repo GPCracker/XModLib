d8c4d9a01073f2cc1ee78147041b83695a1e5ce3
GPCracker <GPCracker@mail.ru>
2015-12-15 12:13:42 +0800

    X-Ray scanner updated and improved.

--- XModLib/XRayScanner.py
+++ XModLib/XRayScanner.py
@@ -18,35 +18,13 @@ import AvatarInputHandler.cameras
 # *************************
 # X-Mod Code Library
 # *************************
-# Nothing
+from .Colliders import Colliders
 
 class XRayScanner(object):
 	@staticmethod
-	def getActiveVehicles(fltr = None):
-		for vehicleID in filter(fltr, BigWorld.player().arena.vehicles.iterkeys()):
-			if vehicleID == BigWorld.player().playerVehicleID:
-				continue
-			vehicle = BigWorld.entity(vehicleID)
-			if vehicle is not None and vehicle.isStarted:
-				activeVehicles.append(vehicle)
-		return activeVehicles
-
-	@staticmethod
-	def scanXRay(scanStart, scanDir, maxDistance, entities, skipGun = False):
-		collisionResult = min(filter(lambda collisionResult: collisionResult[1] is not None, [(entity, entity.collideSegment(scanStart, scanStart + scanDir * maxDistance, skipGun)) for entity in entities]), key = lambda collisionResult: collisionResult[1].dist)
-		return {
-			'scanStart': scanStart,
-			'scanDir': scanDir,
-			'distance': collisionResult[1].dist,
-			'hitPoint': scanStart + scanDir * collisionResult[1].dist,
-			'entity': collisionResult[0],
-			'hitAngleCos': collisionResult[1].hitAngleCos,
-			'armor': collisionResult[1].armor
-		}
-
-	@classmethod
-	def getTarget(sclass, maxDistance = 720, fltr = None, skipGun = False):
+	def getTarget(filterID=None, filterVehicle=None, maxDistance=720, skipGun=False):
 		scanDir, scanStart = AvatarInputHandler.cameras.getWorldRayAndPoint(*BigWorld.player().inputHandler.ctrl.getAim().offset())
 		scanDir.normalise()
-		scanResult = sclass.scanXRay(scanStart, scanDir, maxDistance, sclass.getActiveVehicles(fltr), skipGun)
-		return scanResult['entity'] if scanResult is not None else None
+		scanStop = scanStart + scanDir * maxDistance
+		scanResult = Colliders.collideVehicles(Colliders.getVisibleVehicles(filterID, filterVehicle), scanStart, scanStop, skipGun)
+		return scanResult[1] if scanResult is not None else None
