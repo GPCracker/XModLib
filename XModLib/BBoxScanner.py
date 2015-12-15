be971fdef108a582d92c728a5138630e6740e9ab
GPCracker <GPCracker@mail.ru>
2015-12-15 12:16:07 +0800

    BBox scanner updated and optimized.

--- XModLib/BBoxScanner.py
+++ XModLib/BBoxScanner.py
@@ -9,6 +9,7 @@
 # BigWorld
 # *************************
 import BigWorld
+import Math
 
 # *************************
 # WoT Client
@@ -18,33 +19,22 @@ import AvatarInputHandler.cameras
 # *************************
 # X-Mod Code Library
 # *************************
+from .Colliders import Colliders
 from .Geometry import BoundingBox
 
 class BBoxScanner(object):
-	@staticmethod
-	def getActiveVehicles(fltr = None):
-		for vehicleID in filter(fltr, BigWorld.player().arena.vehicles.iterkeys()):
-			if vehicleID == BigWorld.player().playerVehicleID:
-				continue
-			vehicle = BigWorld.entity(vehicleID)
-			if vehicle is not None and vehicle.isStarted:
-				activeVehicles.append(vehicle)
-		return activeVehicles
-
-	@staticmethod
-	def getVehicleBoundingBox(vehicle, scalar = 1.0):
-		vehicleBB = BoundingBox.getModelBoundingBox(vehicle.model)
-		vehicleBB.scale(scalar)
-		return vehicleBB
+	@classmethod
+	def scanTarget(sclass, scanStart, scanDir, entities, scalar=1.0):
+		unitBB = BoundingBox.getUnitBoundingBox()
+		unitBB.scale(scalar)
+		def scanTransform(entity, scanStart, scanDir):
+			matrix = Math.Matrix(Math.MatrixInverse(entity.model.bounds))
+			return matrix.applyPoint(scanStart), matrix.applyVector(scanDir)
+		targets = filter(lambda entity: unitBB.intersectRayPrimaryCheck(*scanTransform(entity, scanStart, scanDir)), entities)
+		return targets[0] if len(targets) == 1 else None
 
 	@classmethod
-	def scanTarget(sclass, scanStart, scanDir, maxDistance, entities, scalar = 1.0):
-		scanStop = scanStart + scanDir * maxDistance
-		collisionResults = filter(lambda collisionResult: collisionResult[1], [(entity, sclass.getVehicleBoundingBox(entity, scalar).intersectSegment(scanStart, scanStop)) for entity in entities])
-		return collisionResults[0][0] if len(collisionResults) == 1 else None
-
-	@classmethod
-	def getTarget(sclass, maxDistance = 720, scalar = 1.0, fltr = None):
+	def getTarget(sclass, filterID=None, filterVehicle=None, scalar=1.0):
 		scanDir, scanStart = AvatarInputHandler.cameras.getWorldRayAndPoint(*BigWorld.player().inputHandler.ctrl.getAim().offset())
 		scanDir.normalise()
-		return sclass.scanTarget(scanStart, scanDir, maxDistance, sclass.getActiveVehicles(fltr), scalar)
+		return sclass.scanTarget(scanStart, scanDir, Colliders.getVisibleVehicles(filterID, filterVehicle), scalar)
