796110827f5081a4e0e5e9ec1f56ea024b6ed412
GPCracker <GPCracker@mail.ru>
2016-01-10 21:01:10 +0800

    HookUtils property hook implemented.

--- XModLib/HookUtils.py
+++ XModLib/HookUtils.py
@@ -22,6 +22,9 @@ import functools
 from .Helpers import ReleaseEvent as HookEvent
 
 class HookFunction(object):
+	PROPERTY_ACTION_GET = 'fget'
+	PROPERTY_ACTION_SET = 'fset'
+	PROPERTY_ACTION_DEL = 'fdel'
 	CALL_ORIGIN_BEFORE_HOOK = 0x0
 	CALL_HOOK_BEFORE_ORIGIN = 0x1
 	CALL_ORIGIN_INSIDE_HOOK = 0x2
@@ -83,6 +86,33 @@ class HookFunction(object):
 		return hook
 
 	@classmethod
+	def makePropertyHook(sclass, target, method, varname, hook, action=PROPERTY_ACTION_SET, calltype=CALL_ORIGIN_BEFORE_HOOK, active=True):
+		if not isinstance(target, (types.TypeType, types.ClassType)):
+			raise TypeError('Property hook may be used only on classes, not on instances.')
+		if hasattr(target, method):
+			iproperty = getattr(target, method)
+			if not isinstance(iproperty, property):
+				raise TypeError('This class variable is already defined and is not a property.')
+		else:
+			iproperty = property(
+				functools.partial(lambda instance, varname: getattr(instance, varname), varname=varname),
+				functools.partial(lambda instance, value, varname: setattr(instance, varname, value), varname=varname),
+				functools.partial(lambda instance, varname: delattr(instance, varname), varname=varname)
+			)
+			setattr(target, method, iproperty)
+		if action not in (sclass.PROPERTY_ACTION_GET, sclass.PROPERTY_ACTION_SET, sclass.PROPERTY_ACTION_DEL):
+			raise ValueError('Incorrect action value.')
+		kwargs = {
+			'fget': iproperty.fget,
+			'fset': iproperty.fset,
+			'fdel': iproperty.fdel,
+			'doc': iproperty.__doc__
+		}
+		kwargs[action] = sclass(kwargs[action], hook, calltype, active)
+		setattr(target, method, property(**kwargs))
+		return hook
+
+	@classmethod
 	def makeMethodHookOnEvent(sclass, event, target, method, hook, calltype=CALL_ORIGIN_BEFORE_HOOK, active=True):
 		event += functools.partial(sclass.makeMethodHook, target, method, hook, calltype=calltype, active=active)
 		return hook
@@ -98,6 +128,11 @@ class HookFunction(object):
 		return hook
 
 	@classmethod
+	def makePropertyHookOnEvent(sclass, event, target, method, varname, hook, action=PROPERTY_ACTION_SET, calltype=CALL_ORIGIN_BEFORE_HOOK, active=True):
+		event += functools.partial(sclass.makePropertyHook, target, method, varname, hook, action=action, calltype=calltype, active=active)
+		return hook
+
+	@classmethod
 	def methodHook(sclass, target, method, calltype=CALL_ORIGIN_BEFORE_HOOK, active=True):
 		return functools.partial(sclass.makeMethodHook, target, method, calltype=calltype, active=active)
 
@@ -110,6 +145,10 @@ class HookFunction(object):
 		return functools.partial(sclass.makeClassMethodHook, target, method, calltype=calltype, active=active)
 
 	@classmethod
+	def propertyHook(sclass, target, method, varname, action=PROPERTY_ACTION_SET, calltype=CALL_ORIGIN_BEFORE_HOOK, active=True):
+		return functools.partial(sclass.makePropertyHook, target, method, varname, action=action, calltype=calltype, active=active)
+
+	@classmethod
 	def methodHookOnEvent(sclass, event, target, method, calltype=CALL_ORIGIN_BEFORE_HOOK, active=True):
 		return functools.partial(sclass.makeMethodHookOnEvent, event, target, method, calltype=calltype, active=active)
 
@@ -120,3 +159,7 @@ class HookFunction(object):
 	@classmethod
 	def classMethodHookOnEvent(sclass, event, target, method, calltype=CALL_ORIGIN_BEFORE_HOOK, active=True):
 		return functools.partial(sclass.makeClassMethodHookOnEvent, event, target, method, calltype=calltype, active=active)
+
+	@classmethod
+	def propertyHookOnEvent(sclass, event, target, method, varname, action=PROPERTY_ACTION_SET, calltype=CALL_ORIGIN_BEFORE_HOOK, active=True):
+		return functools.partial(sclass.makePropertyHookOnEvent, event, target, method, varname, action=action, calltype=calltype, active=active)
