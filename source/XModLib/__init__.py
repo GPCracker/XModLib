906e5a99a96fd63f85b04d7dc3b5760e577f025a
GPCracker <GPCracker@mail.ru>
2016-01-28 00:28:03 +0300

    Module header changed. Version control implemented.

--- source/XModLib/__init__.py
+++ source/XModLib/__init__.py
@@ -1,23 +1,22 @@
-# Authors: GPCracker
+__application__ = ['X-Mod Code Library', 'XModLib']
+__official_topic__ = None
+__authors__ = ['GPCracker']
+__version__ = '<version>'
+__client__ = [['ru'], '0.9.13', None]
 
-# *************************
-# Python
-# *************************
-# Nothing
+if __name__ == '__main__':
+	appInfo = '{appname} ({appshort}) {version} ({client} {clusters}) by {authors}'.format(
+		appname = __application__[0],
+		appshort = __application__[1],
+		version = __version__,
+		client = __client__[1],
+		clusters = ', '.join(__client__[0]).upper(),
+		authors = ', '.join(__authors__)
+	)
+	print appInfo
+	from time import sleep
+	sleep(len(appInfo) * 0.06)
+	exit()
 
-# *************************
-# BigWorld
-# *************************
-# Nothing
-
-# *************************
-# WoT Client
-# *************************
-# Nothing
-
-# *************************
-# X-Mod Code Library
-# *************************
-# Nothing
-
-pass
+def isCompatibleLibVersion(application=[[None, None]], xmodlib=__version__.split('#')):
+	return any([all([version[0] is None or xmodlib[0] == version[0], version[1] is None or xmodlib[1] == version[1]]) for version in application])
