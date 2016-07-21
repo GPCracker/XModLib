28c1dae746816797c33dcf8ce6ab0dadfd758306
GPCracker <GPCracker@mail.ru>
2016-07-21 21:56:54 +0300

    Line endings fixed.

--- source/XModLib/ResMgrUtils.py
+++ source/XModLib/ResMgrUtils.py
@@ -30,3 +30,7 @@ class ResMgrUtils(object):
 	def resolve_path(path):
 		path = os.path.relpath(ResMgr.resolveToAbsolutePath(path)).replace(os.sep, '/')
 		return path + ('/' if os.path.isdir(path) else '')
+
+	@classmethod
+	def basepath(sclass, path):
+		return sclass.join_path(sclass.resolve_path(path), os.path.relpath('.', path))
