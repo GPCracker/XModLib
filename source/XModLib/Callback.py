4959b9b75f1b9279b068b3b430968a8aa4d21ef6
GPCracker <GPCracker@mail.ru>
2016-08-14 18:08:43 +0300

    Module Callback updated. CallbackLoop class added.

--- source/XModLib/Callback.py
+++ source/XModLib/Callback.py
@@ -57,3 +57,55 @@ class Callback(object):
 	def __del__(self):
 		self.cancelCallback(self.__cbID)
 		return
+
+class CallbackLoop(object):
+	CALLBACK_FIRST = 0x0
+	CALL_FIRST = 0x1
+	CALL_ONLY = 0x2
+
+	def __init__(self, interval, function, calltype=CALLBACK_FIRST):
+		self._interval = interval
+		self._function = function
+		self._calltype = calltype
+		self._callback = None
+		return
+
+	@property
+	def isActive(self):
+		return self._callback is not None
+
+	def _callloop(self):
+		if self._calltype == self.CALLBACK_FIRST:
+			self._callback = Callback(self._interval, Callback.getMethodProxy(self._callloop)) if self.isActive else None
+			if self.isActive:
+				self._function()
+		elif self._calltype == self.CALL_FIRST:
+			if self.isActive:
+				self._function()
+			self._callback = Callback(self._interval, Callback.getMethodProxy(self._callloop)) if self.isActive else None
+		elif self._calltype == self.CALL_ONLY:
+			if self.isActive:
+				self._function()
+			self._callback = None
+		else:
+			raise ValueError('Incorrect call type.')
+		return
+
+	def start(self, delay=None):
+		if self.isActive:
+			raise RuntimeError('Callback loop is already started.')
+		self._callback = Callback(delay if delay is not None else self._interval, Callback.getMethodProxy(self._callloop))
+		return
+
+	def stop(self):
+		if not self.isActive:
+			raise RuntimeError('Callback loop is not active.')
+		self._callback = None
+		return
+
+	def __repr__(self):
+		return 'CallbackLoop(interval={!r}, function={!r}, calltype={!r})'.format(self._interval, self._function, self._calltype)
+
+	def __del__(self):
+		self._callback = None
+		return
