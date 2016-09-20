0a21a9d748e842a64fd48e8fac559ebfd1f2b224
GPCracker <GPCracker@mail.ru>
2016-09-20 11:26:10 +0300

    Module AGScanners set to use module VehicleBounds.

--- source/XModLib/AGScanners.py
+++ source/XModLib/AGScanners.py
@@ -21,6 +21,7 @@ import AvatarInputHandler.cameras
 # *************************
 from .Colliders import Colliders
 from .MathUtils import MathUtils
+from .VehicleBounds import VehicleBounds
 from .AnalyticGeometry import MatrixBoundingBox, MatrixBoundingEllipse
 
 class BoundingScanner(object):
@@ -29,8 +30,12 @@ class BoundingScanner(object):
 		raise NotImplementedError
 		return
 
-	@classmethod
-	def getScaleMatrix(sclass, scalar=1.0):
+	@staticmethod
+	def getVehicleBounds(entity):
+		return getattr(entity, 'collisionBounds', None) or VehicleBounds.getVehicleBoundsMatrixProvider(entity)
+
+	@staticmethod
+	def getScaleMatrix(scalar=1.0):
 		scaleMatrix = MathUtils.getScaleMatrix(Math.Vector3(1.0, 1.0, 1.0).scale(scalar))
 		scaleMatrix.postMultiply(MathUtils.getTranslationMatrix(Math.Vector3(-0.5, -0.5, -0.5).scale(scalar - 1.0)))
 		return scaleMatrix
@@ -41,15 +46,20 @@ class BoundingScanner(object):
 		return targets[0] if len(targets) == 1 else None
 
 	@classmethod
-	def getTargets(sclass, filterID=None, filterVehicle=None, maxDistance=720.0, scalar=1.0, skipPlayer=True):
+	def getTargets(sclass, filterID=None, filterVehicle=None, maxDistance=720.0, scalar=1.0, skipPlayer=True, entities=None):
 		scanDir, scanStart = AvatarInputHandler.cameras.getWorldRayAndPoint(*BigWorld.player().inputHandler.ctrl._aimOffset)
 		scanDir.normalise()
 		scanStop = scanStart + scanDir * maxDistance
-		return sclass.scanTargets(scanStart, scanStop, Colliders.getVisibleVehicles(filterID, filterVehicle, skipPlayer), scalar)
+		return sclass.scanTargets(
+			scanStart,
+			scanStop,
+			Colliders.getVisibleVehicles(filterID, filterVehicle, skipPlayer) if entities is None else entities,
+			scalar
+		)
 
 	@classmethod
-	def getTarget(sclass, filterID=None, filterVehicle=None, maxDistance=720.0, scalar=1.0, skipPlayer=True):
-		targets = sclass.getTargets(filterID, filterVehicle, maxDistance, scalar, skipPlayer)
+	def getTarget(sclass, filterID=None, filterVehicle=None, maxDistance=720.0, scalar=1.0, skipPlayer=True, entities=None):
+		targets = sclass.getTargets(filterID, filterVehicle, maxDistance, scalar, skipPlayer, entities)
 		return targets[0] if len(targets) == 1 else None
 
 class BBoxScanner(BoundingScanner):
@@ -58,7 +68,7 @@ class BBoxScanner(BoundingScanner):
 		scaleMatrix = sclass.getScaleMatrix(scalar)
 		matrixBoundingBox = MatrixBoundingBox.new()
 		def checkEntity(entity):
-			matrixBoundingBox.iBounds = Math.MatrixInverse(MathUtils.getMatrixProduct(scaleMatrix, entity.model.bounds))
+			matrixBoundingBox.iBounds = Math.MatrixInverse(MathUtils.getMatrixProduct(scaleMatrix, sclass.getVehicleBounds(entity)))
 			return matrixBoundingBox.collisionSegment(scanStart, scanStop)
 		return filter(checkEntity, entities)
 
@@ -68,6 +78,6 @@ class BEllipseScanner(BoundingScanner):
 		scaleMatrix = sclass.getScaleMatrix(scalar)
 		matrixBoundingEllipse = MatrixBoundingEllipse.new()
 		def checkEntity(entity):
-			matrixBoundingEllipse.iBounds = Math.MatrixInverse(MathUtils.getMatrixProduct(scaleMatrix, entity.model.bounds))
+			matrixBoundingEllipse.iBounds = Math.MatrixInverse(MathUtils.getMatrixProduct(scaleMatrix, sclass.getVehicleBounds(entity)))
 			return matrixBoundingEllipse.collisionSegment(scanStart, scanStop)
 		return filter(checkEntity, entities)
