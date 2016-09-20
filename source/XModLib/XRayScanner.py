04b7fc9733d3693e46756d24b3f38a057f81188c
GPCracker <GPCracker@mail.ru>
2016-09-20 11:32:37 +0300

    Module XRayScanner now use cached entity list.

--- source/XModLib/XRayScanner.py
+++ source/XModLib/XRayScanner.py
@@ -22,9 +22,14 @@ from .Colliders import Colliders
 
 class XRayScanner(object):
 	@staticmethod
-	def getTarget(filterID=None, filterVehicle=None, maxDistance=720.0, skipGun=False, skipPlayer=True):
+	def getTarget(filterID=None, filterVehicle=None, maxDistance=720.0, skipGun=False, skipPlayer=True, entities=None):
 		scanDir, scanStart = AvatarInputHandler.cameras.getWorldRayAndPoint(*BigWorld.player().inputHandler.ctrl._aimOffset)
 		scanDir.normalise()
 		scanStop = scanStart + scanDir * maxDistance
-		scanResult = Colliders.collideVehicles(Colliders.getVisibleVehicles(filterID, filterVehicle, skipPlayer), scanStart, scanStop, skipGun)
+		scanResult = Colliders.collideVehicles(
+			Colliders.getVisibleVehicles(filterID, filterVehicle, skipPlayer) if entities is None else entities,
+			scanStart,
+			scanStop,
+			skipGun
+		)
 		return scanResult[1] if scanResult is not None else None
