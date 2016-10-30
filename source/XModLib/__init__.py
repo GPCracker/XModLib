10450f6d6740dc4a80eb57e90a26472fd774c6ea
GPCracker <GPCracker@mail.ru>
2016-10-30 15:12:09 +0300

    Small format updates.

--- source/XModLib/__init__.py
+++ source/XModLib/__init__.py
@@ -4,8 +4,11 @@ __authors__ = ['GPCracker']
 __version__ = '<version>'
 __client__ = [['ru'], '0.9.15.2', None]
 
+# *************************
+# Application info
+# *************************
 if __name__ == '__main__':
-	appInfo = '{appname} ({appshort}) {version} ({client} {clusters}) by {authors}'.format(
+	applicationInfo = '{appname} ({appshort}) {version} ({client} {clusters}) by {authors}'.format(
 		appname = __application__[0],
 		appshort = __application__[1],
 		version = __version__,
@@ -13,10 +16,12 @@ if __name__ == '__main__':
 		clusters = ', '.join(__client__[0]).upper(),
 		authors = ', '.join(__authors__)
 	)
-	print appInfo
-	from time import sleep
-	sleep(len(appInfo) * 0.06)
+	print applicationInfo
+	__import__('time').sleep(len(applicationInfo) * 0.05)
 	exit()
 
+# *************************
+# Compatibility test
+# *************************
 def isCompatibleLibVersion(application=[[None, None]], xmodlib=__version__.split('#')):
 	return any([all([version[0] is None or xmodlib[0] == version[0], version[1] is None or xmodlib[1] == version[1]]) for version in application])
