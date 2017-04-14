f99ab87d7b2969ecb1acb18f669796ed990227eb
GPCracker <GPCracker@mail.ru>
2017-04-14 16:10:20 +0800

    New functions for accessing ResMgr virtual file system added.

--- source/XModLib/EngineUtils.py
+++ source/XModLib/EngineUtils.py
@@ -28,3 +28,29 @@ def resolveResMgrPath(path):
 
 def getResMgrBasePath(path):
 	return joinResMgrPath(resolveResMgrPath(path), os.path.relpath('.', path))
+
+def getResMgrBinaryFileContent(path):
+	if ResMgr.isFile(path):
+		section = ResMgr.openSection(path)
+		if section is not None:
+			return section.asBinary
+	return None
+
+def getResMgrDirectoryContent(path):
+	uniques = lambda entries: sorted(set(entries), key=entries.index)
+	if ResMgr.isDir(path):
+		section = ResMgr.openSection(path)
+		if section is not None:
+			return uniques(section.keys())
+	return None
+
+def walkResMgrTree(dirpath):
+	entries = getResMgrDirectoryContent(dirpath)
+	if entries is not None:
+		dirnames = filter(lambda subpath: ResMgr.isDir(joinResMgrPath(dirpath, subpath)), entries)
+		filenames = filter(lambda subpath: ResMgr.isFile(joinResMgrPath(dirpath, subpath)), entries)
+		yield dirpath, dirnames, filenames
+		for dirname in dirnames:
+			for block in walkResMgrTree(joinResMgrPath(dirpath, dirname)):
+				yield block
+	return
