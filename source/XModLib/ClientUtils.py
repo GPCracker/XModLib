6358d29a2f90cb02dca803945267470a27716bd3
Vladislav Ignatenko <gpcracker@mail.ru>
2017-11-20 17:25:05 +0800

    Module ClientUtils added.

---
+++ source/XModLib/ClientUtils.py
@@ -0,0 +1,35 @@
+# Authors: GPCracker
+
+# *************************
+# Python
+# *************************
+import string
+import itertools
+
+# *************************
+# BigWorld
+# *************************
+# Nothing
+
+# *************************
+# WoT Client
+# *************************
+import helpers
+import constants
+
+# *************************
+# X-Mod Library
+# *************************
+from . import Versioning
+
+def getClientVersion():
+	realm = constants.CURRENT_REALM.lower()
+	version = helpers.getShortClientVersion().strip()
+	return Versioning.ProvidedClientVersion(realm, Versioning.Version.parseVersion(version))
+
+def isCompatibleClientVersion(required=(None, None)):
+	def parseRequiredVersion(realms, version):
+		realms = tuple(itertools.imap(string.lower, realms)) if realms not in ('any-realm', None) else ()
+		version = Versioning.Version.parseVersion(version) if version not in ('any-version', None) else ()
+		return Versioning.RequiredClientVersion(realms, version)
+	return getClientVersion().isClientVersionCompatible(parseRequiredVersion(*required))
