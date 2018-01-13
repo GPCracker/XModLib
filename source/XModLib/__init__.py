deb52c15c12954e0fad40fcb6a857fc247c245b8
Vladislav Ignatenko <gpcracker@mail.ru>
2018-01-13 14:14:10 +0800

    New application header implemented.

--- source/XModLib/__init__.py
+++ source/XModLib/__init__.py
@@ -2,23 +2,24 @@ __application__ = ('X-Mod Library', 'XModLib', 'GPCracker.XModLib')
 __official_topic__ = None
 __authors__ = ('GPCracker', )
 __version__ = ('<<version>>', None)
-__client__ = (('ru', ), '<<client>>', None)
+__client__ = (('ru', ), '<<client>>')
 
 # ---------------------- #
 #    Application info    #
 # ---------------------- #
 if __name__ == '__main__':
-	applicationInfo = '{appname} ({appshort}) {version} ({client} {clusters}) by {authors}'.format(
+	appinfo = '{appname} ({appid}) {version} ({client} {clusters}) by {authors}'.format(
 		appname = __application__[0],
-		appshort = __application__[1],
+		appid = __application__[2],
 		version = __version__[0],
 		client = __client__[1],
 		clusters = ', '.join(__client__[0]).upper(),
 		authors = ', '.join(__authors__)
 	)
-	print applicationInfo
-	__import__('time').sleep(len(applicationInfo) * 0.05)
-	exit()
+	import sys, time
+	print >> sys.stdout, appinfo
+	time.sleep(len(appinfo) * 0.05)
+	sys.exit(0)
 
 # -------------------------------------- #
 #    X-Mod Library compatibility test    #
